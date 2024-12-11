711cfea7dd38a3dce3595c54565da3d571b09c50
mizanur-rahaman-bd <sumon1581@gmail.com>
2024-12-11 22:39:53 +0600

    msgrunning

--- src/Components/MsgBoxCompo/MsgBoxCompo.jsx
+++ src/Components/MsgBoxCompo/MsgBoxCompo.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import InputEmoji from "react-input-emoji";
+import "./MsgBoxCompo.css";
 
 const MsgBoxCompo = () => {
-  return (
-    <div>MsgBoxCompo</div>
-  )
-}
+  const [text, setText] = useState("");
 
-export default MsgBoxCompo
+  function handleOnEnter(text) {
+    console.log("enter", text);
+  }
+
+  return (
+    <>
+      <section className="msgBox">
+        <div className="msgBoxBar">
+          <div className="userPhoto">
+            <img src="" alt="userPhoto" />
+          </div>
+          <h2>Lorem, ipsum dolor.</h2>
+        </div>
+        <div className="userMsg">
+          <div className="receive_msg">Lorem ipsum dolor sit amet.</div>
+          <div className="send_msg">Lorem ipsum dolor sit amet.</div>
+        </div>
+        <InputEmoji
+          value={text}
+          onChange={setText}
+          cleanOnEnter
+          onEnter={handleOnEnter}
+          placeholder="Type a message"
+        />
+      </section>
+    </>
+  );
+};
+
+export default MsgBoxCompo;
